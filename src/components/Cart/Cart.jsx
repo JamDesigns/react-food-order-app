536bc8d1737a3acad464be67437bf645ef6945f3
Jose Mosquera <joseangel.mosquera@gmail.com>
2023-06-10 18:20:41 +0200

    Add checkout form and remote http Data access

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,66 +1,166 @@
-import { useContext } from "react";
+import { useContext, useState, Fragment } from "react";
 
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+import useHttp from "../../hooks/use-https";
+import CircleLoader from "react-spinners/CircleLoader";
+
 import classes from "./Cart.module.css";
 
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "#53f553",
+};
+
 const Cart = (props) => {
-    const cartCtx = useContext(CartContext);
+  const [isDidSubmit, setIsDidSubmit] = useState(false);
+  const { isLoading, error, sendRequest: sentCartRequest } = useHttp();
 
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItems = cartCtx.items.length > 0;
+  const [isCheckout, setIsCheckout] = useState(false);
 
-    const cartItemRemoveHandler = (id) => {
-        cartCtx.removeItem(id);
-    };
+  const cartCtx = useContext(CartContext);
 
-    const cartItemAddHandler = (item) => {
-        cartCtx.addItem({ ...item, amount: 1 });
-    };
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-    const cartItems = (
-        <ul className={classes["cart-items"]}>
-            {cartCtx.items.map((item) => (
-                <CartItem
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    amount={item.amount}
-                    price={item.price}
-                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={cartItemAddHandler.bind(null, item)}
-                />
-            ))}
-        </ul>
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const clearCartHandler = () => {
+    cartCtx.clearCart();
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = (userData) => {
+    sentCartRequest({
+      url: "https://react-http-26100-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+      method: "POST",
+      body: {
+        user: userData,
+        orderedItems: cartCtx.items,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    cartCtx.clearCart();
+    setIsCheckout(false);
+    setIsDidSubmit(true);
+  };
+
+  const modalActions = (
+    <div className={classes.actions}>
+      {hasItems && (
+        <button className={classes["button--alt"]} onClick={clearCartHandler}>
+          Clear Cart
+        </button>
+      )}
+      <button
+        className={`${
+          isDidSubmit || !hasItems ? classes.button : classes["button--alt"]
+        }`}
+        onClick={props.onHideCart}
+      >
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  const onCancelCheckoutHandler = () => {
+    setIsCheckout(false);
+  };
+
+  let content = (
+    <Fragment>
+      {hasItems ? (
+        cartItems
+      ) : (
+        <div className={classes["cart-empty"]}>Your cart is empty</div>
+      )}
+      <div
+        className={`${classes.total} ${!hasItems ? classes["cart-empty"] : ""}`}
+      >
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && (
+        <Checkout
+          onCancel={onCancelCheckoutHandler}
+          onConfirm={submitOrderHandler}
+        />
+      )}
+      {!isCheckout && modalActions}
+    </Fragment>
+  );
+
+  if (isLoading) {
+    content = (
+      <CircleLoader
+        color={"#53f553"}
+        loading={isLoading}
+        cssOverride={override}
+        aria-label="Loading meals..."
+        data-testid="loader"
+      />
     );
-    return (
-        <Modal onClick={props.onHideCart}>
-            <div className={classes.title}>{props.title}</div>
-            {hasItems ? (
-                cartItems
-            ) : (
-                <div className={classes["cart-empty"]}>Your cart is empty</div>
-            )}
-            <div
-                className={`${classes.total} ${
-                    !hasItems ? classes["cart-empty"] : ""
-                }`}
-            >
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            <div className={classes.actions}>
-                <button
-                    className={classes["button--alt"]}
-                    onClick={props.onHideCart}
-                >
-                    Close
-                </button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
-        </Modal>
+  }
+
+  if (error) {
+    content = (
+      <Fragment>
+        <p className={classes.invalid}>{error}</p>
+      </Fragment>
     );
+  }
+
+  if (isDidSubmit) {
+    content = (
+      <Fragment>
+        <p className={classes["cart-empty"]}>Successfully sent the order!</p>
+        {modalActions}
+      </Fragment>
+    );
+  }
+
+  return (
+    <Modal onClick={props.onHideCart}>
+      <div className={classes.title}>{props.title}</div>
+      {content}
+    </Modal>
+  );
 };
 
 export default Cart;
--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,27 +1,82 @@
+import { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
-import DUMMY_MEALS from "../../data/dummy-meals";
+import MealItem from "./MealItem/MealItem";
+import useHttp from "../../hooks/use-https";
+import CircleLoader from "react-spinners/CircleLoader";
 
 import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "#53f553",
+};
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
-        <MealItem
-            key={meal.id}
-            id={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-        />
-    ));
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
 
-    return (
-        <section className={classes.meals}>
-            <Card className="Available-Meal">
-                <ul>{mealsList}</ul>
-            </Card>
-        </section>
+  useEffect(() => {
+    const mealsList = (responseData) => {
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals(
+      {
+        url: "https://react-http-26100-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
+      },
+      mealsList,
     );
+  }, [fetchMeals]);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  let content = <ul>{mealsList}</ul>;
+
+  if (isLoading) {
+    content = (
+      <CircleLoader
+        color={"#53f553"}
+        loading={isLoading}
+        cssOverride={override}
+        aria-label="Loading meals..."
+        data-testid="loader"
+      />
+    );
+  }
+
+  if (mealsList.length === 0) {
+    content = <p className={classes["no-data"]}>No data has been found</p>;
+  }
+
+  if (error) {
+    content = <p className={classes["text-error"]}>{error}</p>;
+  }
+
+  return (
+    <section className={classes.meals}>
+      <Card className="Available-Meal">{content}</Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
